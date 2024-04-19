d311f87e641b8b9b4edae3351a1aa48e416a1a1e
Unika Mureithi <unikamureithi@gmail.com>
2024-04-19 19:06:29 +0300

    feat: add function handleDelete Closes#16

--- client/src/components/ViewTasks.tsx
+++ client/src/components/ViewTasks.tsx
@@ -3,7 +3,9 @@ import { APIType, Task } from '../@types/types'
 import { Box, Button, Checkbox, Chip, Typography } from '@mui/material'
 
 const ViewTasks: React.FC<APIType> = ({API_URL}) => {
+
     const [tasks, setTasks] = useState<Task[]>([])
+
     useEffect(()=>{
         const fetchData = async()=>{
             try{
@@ -19,6 +21,14 @@ const ViewTasks: React.FC<APIType> = ({API_URL}) => {
 
     },[API_URL])
 
+    const handleDelete = async(id:number)=>{
+        const deleteOption = {method: 'DELETE'}
+        const reqUrl = `${API_URL}/${id}`
+        const result = await fetch(reqUrl, deleteOption)
+        const data = await result.json()
+        setTasks(data)
+    }
+
     return (
         <div>
             {tasks.map((task)=>(
@@ -26,21 +36,21 @@ const ViewTasks: React.FC<APIType> = ({API_URL}) => {
                     <Typography fontWeight={700}>Task {task.id}</Typography>
                     <Typography>{task.title}</Typography>
                     {
-                    task.priority === 1 ? <Chip label="Urgent" sx={{backgroundColor:"#FF6A06", color:"#fff"}}/>:
-                    task.priority === 2 ? <Chip label="Medium" sx={{backgroundColor:"#007A00", color:"#fff"}}/>:
-                    <Chip label="Minor" sx={{backgroundColor:"#808080", color:"#fff"}}/>
-                    }
+                    task.priority === 1 ? <Chip label="Urgent" sx={{backgroundColor:"#FF6A06", color:"#fff", width:"80px"}}/>:
+                    task.priority === 2 ? <Chip label="Medium" sx={{backgroundColor:"#007A00", color:"#fff", width:"80px"}}/>:
+                    <Chip label="Minor" sx={{backgroundColor:"#808080", color:"#fff", width:"80px"}}/>                    }
                     <Typography>{task.priority}</Typography>
                     <Typography>{task.createdAt}</Typography>
                     {task.done === true ? <Checkbox defaultChecked sx={{mt:-1}}/> : <Checkbox sx={{mt:-1}}/>}
                     <Typography>{task.doneAt}</Typography>
                     <Box display="flex" justifyContent="flex-end" flexGrow={1} gap={5}>
                         <Button variant='contained'>Edit</Button>
-                        <Button variant='contained' style={{backgroundColor:"#C30010"}}>Delete</Button>
+                        <Button variant='contained' style={{backgroundColor:"#C30010"}} onClick={()=>{handleDelete(task.id)}}>Delete</Button>
                     </Box>
                 </Box>
 
             ))}
+            <Button variant='contained' sx={{mt:5, py:2, px:4,backgroundColor:"#111111"}}>Create a new Task</Button>
         </div>
     )
 }
